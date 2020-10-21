8acac640d5ec95639acc691bc07b9e434c820aa8
The Sashko <sashko.me>
2020-10-21 14:44:49 +0300

    add phan tests

---
+++ protected/tests/.phan/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'target_php_version' => '7.2',
+
+    'directory_list' => [
+        '../'
+    ],
+
+    'exclude_analysis_directory_list' => [
+        '../tests',
+        '../core'
+    ],
+];
