69bfe86be540847f9cf81fe2f4bce4a2d9336e8c
The Sashko <sashko.me>
2020-10-26 18:18:50 +0200

    add captcha cron

--- protected/controllers/CronController.php
+++ protected/controllers/CronController.php
@@ -4,11 +4,28 @@
  */
 class CronController extends CronControllerCore
 {
-    /**
-     * Test Cron Job
-     */
-    public function jobTest(): void
+    public function jobCaptcha(): void
     {
-        $this->getPlugin('logger')->log('Cron Test Job Executed', 'test');
+        $cryptConfig = $this->getConfig('crypt');
+
+        if (
+            !array_key_exists('salt', $cryptConfig) ||
+            empty($cryptConfig['salt'])
+        ) {
+            //to-do
+        }
+
+        $cryptSalt = (string) $cryptConfig['salt'];
+
+        $captchaSettings = [
+            'data_dir_path' => __DIR__.'/../res/captcha',
+            'hash_salt'     => $cryptSalt
+        ];
+
+        $captchaPlugin = $this->getPlugin('captcha');
+        $captchaPlugin->setSettings($captchaSettings);
+        $captchaPlugin->cron();
+
+        // to-do: remove old files
     }
 }
